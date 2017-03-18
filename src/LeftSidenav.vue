6e26ff28f7fc2099a142924da135384af2d61683
Veeravit Harutaipree <bytrio@hotmail.com>
2017-03-18 21:34:39 +0700

    initial

---
+++ src/LeftSidenav.vue
@@ -0,0 +1,41 @@
+<template>
+  <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+    <el-submenu index="1">
+      <template slot="title">Menu</template>
+      <el-menu-item index="2-1" @click.native="index">Index</el-menu-item>
+      <el-menu-item index="2-2" @click.native="newPost">New Post</el-menu-item>
+      <el-menu-item index="2-3" @click.native="logout">Logout</el-menu-item>
+    </el-submenu>
+  </el-menu>
+</template>
+
+<script>
+import router from './router'
+import UsersApi from './api/users.js'
+
+export default {
+  data () {
+    return {
+      activeIndex: '1'
+    }
+  },
+  methods: {
+    handleSelect (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    index () {
+      router.push({ name: 'Posts.index' })
+    },
+    newPost () {
+      router.push({ name: 'Posts.new' })
+    },
+    logout () {
+      UsersApi.logout()
+      router.push({name: 'Users.sign_in'})
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/components/posts/New.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="new-post">
+    <h1>New Post</h1>
+    <label>Name</label>
+    <el-input placeholder="" v-model="post.name"></el-input>
+    <br></br>
+    <label>Content</label>
+    <el-input type="textarea":rows="2" placeholder="" v-model="post.content"></el-input>
+    <br></br>
+    <el-button type="primary" @click.native="createPost">Save</el-button>
+  </div>
+</template>
+
+<script>
+import PostsApi from '../../api/posts.js'
+import router from '../../router'
+
+export default {
+  name: 'new-post',
+  data () {
+    return {
+      post: {
+        name: '',
+        content: ''
+      }
+    }
+  },
+  methods: {
+    createPost () {
+      PostsApi.createPost(this.post,
+        function (_post) {
+          router.push({ name: 'Posts.index' })
+        }
+      )
+    }
+  }
+}
+</script>
+
+<style>
+</style>
